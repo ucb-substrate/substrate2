04acdd7ab6450c24a928256b4df49dd9897e5eee
Rahul Kumar <rahulkumar@berkeley.edu>
2023-06-11 18:21:38 -0700

    Simple change

--- layout/src/lib.rs
+++ layout/src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn sub(left: usize, right: usize) -> usize {
+    left - right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
