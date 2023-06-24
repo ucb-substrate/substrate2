b1513577748148d883c9470011eee6ca1fba334b
Rahul Kumar <rahulkumar@berkeley.edu>
2023-06-24 15:15:29 -0700

    make assertions in test

--- substrate-api/src/pdk/mod.rs
+++ substrate-api/src/pdk/mod.rs
@@ -33,7 +33,7 @@ pub struct Layer {
     gds: Option<(i16, i16)>,
 }
 
-#[derive(Debug, Default, Copy, Clone, Serialize, Deserialize)]
+#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum RoutingKind {
     #[default]
@@ -46,7 +46,6 @@ pub enum RoutingKind {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use toml::from_str;
 
     #[test]
     fn deserialize_example_pdk_toml() {
@@ -57,5 +56,16 @@ mod tests {
         println!("{}", spec);
         let spec = std::fs::read_to_string(spec).unwrap();
         let spec: PdkSpec = toml::from_str(&spec).unwrap();
+        assert_eq!(spec.pdk.name, "example_pdk");
+        assert_eq!(spec.layers.inner["met1"].routing_kind, RoutingKind::Routing);
+        assert_eq!(
+            spec.layers.inner["met1"].elements["drawing"].gds,
+            Some((1, 2))
+        );
+        assert_eq!(
+            spec.layers.inner["met1"].elements["drawing"].name,
+            "met1_drawing",
+        );
+        assert_eq!(spec.layers.inner["via1"].routing_kind, RoutingKind::Cut,);
     }
 }
