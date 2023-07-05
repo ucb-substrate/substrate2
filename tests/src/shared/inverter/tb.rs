911a3768bf730d7fd134310d500560873f008783
Rahul Kumar <rahulkumar@berkeley.edu>
2023-07-05 13:43:40 -0700

    feat(inverter-design): initial inverter design logic (#144)
    * feat(inverter-design): initial inverter design logic
    
    * feat(design): update inverter design script
    
    * add print
    
    * fix(tb): fix inverter tb
    
    * quick fix: hash spectre vsource for deduplication
    
    * feat(spectre): add error presets
    
    * fix(spectre): add missing spaces
    
    * fix transition thresholds
    
    * increase rise/fall times
    
    * change numbers

--- tests/src/shared/inverter/tb.rs
+++ tests/src/shared/inverter/tb.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+
+use rust_decimal::prelude::ToPrimitive;
 use rust_decimal_macros::dec;
 use serde::{Deserialize, Serialize};
 use sky130pdk::corner::Sky130Corner;
@@ -5,10 +8,13 @@ use sky130pdk::Sky130CommercialPdk;
 use spectre::blocks::{Pulse, Vsource};
 use spectre::{Options, Spectre, Tran};
 use substrate::block::Block;
-use substrate::io::Node;
+use substrate::context::Context;
+use substrate::io::{Node, Signal};
 use substrate::ios::TestbenchIo;
 use substrate::pdk::corner::{InstallCorner, Pvt};
 use substrate::schematic::{Cell, HasSchematic};
+use substrate::simulation::data::HasNodeData;
+use substrate::simulation::waveform::{EdgeDir, TimeWaveform, WaveformRef};
 use substrate::simulation::{HasTestbenchSchematicImpl, Testbench};
 
 use super::Inverter;
@@ -16,12 +22,13 @@ use super::Inverter;
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub struct InverterTb {
     pvt: Pvt<Sky130Corner>,
+    dut: Inverter,
 }
 
 impl InverterTb {
     #[inline]
-    pub fn new(pvt: Pvt<Sky130Corner>) -> Self {
-        Self { pvt }
+    pub fn new(pvt: Pvt<Sky130Corner>, dut: Inverter) -> Self {
+        Self { pvt, dut }
     }
 }
 
@@ -48,38 +55,46 @@ impl HasTestbenchSchematicImpl<Sky130CommercialPdk, Spectre> for InverterTb {
         io: &<<Self as Block>::Io as substrate::io::SchematicType>::Data,
         cell: &mut substrate::schematic::TestbenchCellBuilder<Sky130CommercialPdk, Spectre, Self>,
     ) -> substrate::error::Result<Self::Data> {
-        let inv = cell.instantiate(Inverter {
-            nw: 1_200,
-            pw: 2_000,
-            lch: 150,
-        });
-        let vdd = cell.instantiate_tb(Vsource::pulse(Pulse {
+        let inv = cell.instantiate(self.dut);
+
+        let vdd = cell.signal("vdd", Signal);
+        let dout = cell.signal("dout", Signal);
+
+        let vddsrc = cell.instantiate_tb(Vsource::dc(self.pvt.voltage));
+        cell.connect(vddsrc.io().p, vdd);
+        cell.connect(vddsrc.io().n, io.vss);
+
+        let vin = cell.instantiate_tb(Vsource::pulse(Pulse {
             val0: 0.into(),
             val1: self.pvt.voltage,
             delay: Some(dec!(0.1e-9)),
             width: Some(dec!(1e-9)),
-            fall: Some(dec!(40e-15)),
-            rise: Some(dec!(40e-15)),
+            fall: Some(dec!(1e-12)),
+            rise: Some(dec!(1e-12)),
             period: None,
         }));
-        cell.connect(vdd.io().p, inv.io().vdd);
-        cell.connect(vdd.io().n, io.vss);
+        cell.connect(inv.io().din, vin.io().p);
+        cell.connect(vin.io().n, io.vss);
+
+        cell.connect(inv.io().vdd, vdd);
         cell.connect(inv.io().vss, io.vss);
-        Ok(*inv.io().dout)
+        cell.connect(inv.io().dout, dout);
+
+        Ok(dout)
     }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct VdividerTbData {
-    pub vdd: Vec<f64>,
-    pub out: Vec<f64>,
+pub struct InverterTbData {
+    pub tr: f64,
+    pub tf: f64,
 }
 
 impl Testbench<Sky130CommercialPdk, Spectre> for InverterTb {
-    type Output = ();
+    type Output = InverterTbData;
     fn run(
         &self,
-        _cell: &Cell<Self>,
+        cell: &Cell<Self>,
         sim: substrate::simulation::SimController<Sky130CommercialPdk, Spectre>,
     ) -> Self::Output {
         let mut opts = Options::default();
@@ -89,16 +104,75 @@ impl Testbench<Sky130CommercialPdk, Spectre> for InverterTb {
                 opts,
                 Tran {
                     stop: dec!(2e-9),
+                    errpreset: Some(spectre::ErrPreset::Conservative),
                     ..Default::default()
                 },
             )
             .expect("failed to run simulation");
 
-        println!("Output:\n{:?}", output);
+        let vout = output.get_data(&cell.data()).unwrap();
+        let time = output.get_data("time").unwrap();
+        let vout = WaveformRef::new(time, vout);
+        let mut trans = vout.transitions(
+            0.2 * self.pvt.voltage.to_f64().unwrap(),
+            0.8 * self.pvt.voltage.to_f64().unwrap(),
+        );
+        // The input waveform has a low -> high, then a high -> low transition.
+        // So the first transition of the inverter output is high -> low.
+        // The duration of this transition is the inverter fall time.
+        let tf = trans.next().unwrap();
+        assert_eq!(tf.dir(), EdgeDir::Falling);
+        let tf = tf.duration();
+        let tr = trans.next().unwrap();
+        assert_eq!(tr.dir(), EdgeDir::Rising);
+        let tr = tr.duration();
 
-        // VdividerTbData {
-        //     vdd: output.get_data(&cell.data().io().pwr.vdd).unwrap().clone(),
-        //     out: output.get_data(&cell.data().io().out).unwrap().clone(),
-        // }
+        InverterTbData { tf, tr }
+    }
+}
+
+/// Designs an inverter for balanced pull-up and pull-down times.
+///
+/// The NMOS width is kept constant; the PMOS width is swept over
+/// the given range.
+pub struct InverterDesign {
+    /// The fixed NMOS width.
+    pub nw: i64,
+    /// The set of PMOS widths to sweep.
+    pub pw: Vec<i64>,
+    /// The transistor channel length.
+    pub lch: i64,
+}
+
+impl InverterDesign {
+    pub fn run(
+        &self,
+        ctx: &mut Context<Sky130CommercialPdk>,
+        work_dir: impl AsRef<Path>,
+    ) -> Inverter {
+        let work_dir = work_dir.as_ref();
+        let pvt = Pvt::new(Sky130Corner::Tt, dec!(1.8), dec!(25));
+
+        let mut opt = None;
+        for pw in self.pw.iter().copied() {
+            let dut = Inverter {
+                nw: self.nw,
+                pw,
+                lch: self.lch,
+            };
+            let tb = InverterTb::new(pvt, dut);
+            let data = ctx.simulate(tb, work_dir.join(format!("pw{pw}")));
+            println!("Simulating with pw = {pw} gave:\n{:#?}", data);
+            let diff = (data.tr - data.tf).abs();
+            if let Some((pdiff, _)) = opt {
+                if diff < pdiff {
+                    opt = Some((diff, dut));
+                }
+            } else {
+                opt = Some((diff, dut));
+            }
+        }
+
+        opt.unwrap().1
     }
 }
