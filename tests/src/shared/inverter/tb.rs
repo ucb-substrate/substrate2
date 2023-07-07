f894cfda40e7337a720b8bc190c973a51bc2ce8d
Rahul Kumar <rahulkumar@berkeley.edu>
2023-07-06 20:36:11 -0700

    fix(docs): dont hardcode process corner

--- tests/src/shared/inverter/tb.rs
+++ tests/src/shared/inverter/tb.rs
@@ -94,7 +94,7 @@ impl Testbench<Sky130CommercialPdk, Spectre> for InverterTb {
         sim: substrate::simulation::SimController<Sky130CommercialPdk, Spectre>,
     ) -> Self::Output {
         let mut opts = Options::default();
-        sim.pdk.pdk.install_corner(Sky130Corner::Tt, &mut opts);
+        sim.pdk.pdk.install_corner(self.pvt.corner, &mut opts);
         let output = sim
             .simulate(
                 opts,
