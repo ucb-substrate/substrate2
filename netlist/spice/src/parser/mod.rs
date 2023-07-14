eb8129be70574377b00cc5f3774fe84f8e20ec4e
Rahul Kumar <rahulkumar@berkeley.edu>
2023-07-14 15:11:38 -0700

    can parse DFF spice

--- netlist/spice/src/parser/mod.rs
+++ netlist/spice/src/parser/mod.rs
@@ -3,68 +3,148 @@
 #[cfg(test)]
 mod tests;
 
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::fmt::Display;
 use std::ops::{Deref, DerefMut};
+use std::path::Path;
 
+use arcstr::ArcStr;
 use nom::bytes::complete::{take_till, take_while};
 use nom::error::ErrorKind;
 use nom::{IResult, InputTakeAtPosition};
 
 pub type Node = Substr;
 
-#[derive(Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
+#[derive(Clone, Default, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 #[repr(transparent)]
 pub struct Substr(arcstr::Substr);
 
+#[derive(Clone, Default, Eq, PartialEq, Debug)]
 pub struct Parser {
     buffer: Vec<Token>,
     ast: Ast,
+    state: ParseState,
+}
+
+#[derive(Clone, Default, Eq, PartialEq, Debug)]
+enum ParseState {
+    #[default]
+    Top,
+    Subckt(Subckt),
 }
 
 impl Parser {
-    pub fn parse_file() {}
-
-    pub fn parse(&mut self, data: Substr) {
+    pub fn parse_file(path: impl AsRef<Path>) -> Ast {
+        let path = path.as_ref();
+        let s: ArcStr = std::fs::read_to_string(path).unwrap().into();
+        let s = Substr(arcstr::Substr::full(s));
+        let mut parser = Self::default();
+        parser.parse(s);
+        parser.ast
     }
 
-    fn parse_line(&mut self, data: Substr) {
-        let tokens = Tokenizer::new(data);
-        for token in tokens {
-            if token == Token::LineEnd {
-                self.process_buffer();
-            } else {
-                self.buffer.push(token);
+    pub fn parse(&mut self, data: Substr) {
+        let mut tok = Tokenizer::new(data);
+        while let Some(line) = self.parse_line(&mut tok) {
+            match (&mut self.state, line) {
+                (ParseState::Top, Line::SubcktDecl { name, ports }) => {
+                    self.state = ParseState::Subckt(Subckt {
+                        name,
+                        ports,
+                        components: vec![],
+                    });
+                }
+                (ParseState::Top, Line::Component(c)) => {
+                    self.ast.elems.push(Elem::Component(c));
+                }
+                (ParseState::Subckt(ref mut subckt), Line::Component(c)) => {
+                    subckt.components.push(c);
+                }
+                (ParseState::Subckt(ref mut subckt), Line::EndSubckt) => {
+                    let subckt = std::mem::take(subckt);
+                    self.ast.elems.push(Elem::Subckt(subckt));
+                    self.state = ParseState::Top;
+                }
+                _ => panic!("Unexpected line"),
             }
         }
     }
 
-    fn parse_line_inner(&mut self) -> Option<Line> {
+    fn parse_line(&mut self, tok: &mut Tokenizer) -> Option<Line> {
+        while let Some(token) = tok.get() {
+            if token == Token::LineEnd {
+                return Some(self.parse_line_inner());
+            } else {
+                self.buffer.push(token);
+            }
+        }
+
+        None
+    }
+
+    fn parse_line_inner(&mut self) -> Line {
         let line = match self.buffer.first().unwrap() {
             Token::Directive(d) => {
                 if d.eq_ignore_ascii_case(".subckt") {
                     // TODO params
-                    let ports = self.buffer[1..].iter().map(|tok| {
-                        tok.unwrap_ident().clone()
-                    }).collect();
-                    Some(Line::SubcktDecl { ports })
+                    let name = self.buffer[1].unwrap_ident().clone();
+                    let ports = self.buffer[2..]
+                        .iter()
+                        .map(|tok| tok.unwrap_ident().clone())
+                        .collect();
+                    Line::SubcktDecl { name, ports }
                 } else if d.eq_ignore_ascii_case(".ends") {
-                    Some(Line::EndSubckt)
+                    Line::EndSubckt
                 } else {
-                    None
+                    panic!("unexpected directive: `{d}`");
                 }
-            },
+            }
             Token::Ident(id) => {
-                let pos = self.buffer.iter().position(|t| matches!(t, Token::Equals));
-                Some(Line::Component(todo!()))
-            },
-            tok => panic!("Unexpected token: {:?}", tok),
+                let kind = id.chars().next().unwrap().to_ascii_uppercase();
+
+                match kind {
+                    'R' => Line::Component(Component::Res(Res {
+                        name: self.buffer[0].unwrap_ident().substr(1..).clone().into(),
+                        pos: self.buffer[1].unwrap_ident().clone(),
+                        neg: self.buffer[2].unwrap_ident().clone(),
+                        value: self.buffer[3].unwrap_ident().clone(),
+                    })),
+                    'X' => {
+                        let pos = self.buffer.iter().position(|t| matches!(t, Token::Equals));
+                        let child_idx = pos.unwrap_or(self.buffer.len() + 1) - 2;
+                        let child = self.buffer[child_idx].unwrap_ident().clone();
+                        let ports = self.buffer[1..child_idx]
+                            .iter()
+                            .map(|x| x.unwrap_ident().clone())
+                            .collect();
+
+                        let mut params = Params::default();
+                        for i in (child_idx + 1..self.buffer.len()).step_by(3) {
+                            let k = self.buffer[i].unwrap_ident().clone();
+                            assert!(matches!(self.buffer[i + 1], Token::Equals));
+                            let v = self.buffer[i + 2].unwrap_ident().clone();
+                            params.insert(k, v);
+                        }
+
+                        Line::Component(Component::Instance(Instance {
+                            name: self.buffer[0].unwrap_ident().substr(1..).clone().into(),
+                            ports,
+                            child,
+                            params,
+                        }))
+                    }
+                    kind => panic!("unexpected component type: {kind}"),
+                }
+            }
+            tok => panic!("unexpected token: {:?}", tok),
         };
         self.buffer.clear();
         line
     }
 }
 
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
 pub struct Ast {
     /// The list of elements in the SPICE netlist.
     elems: Vec<Elem>,
@@ -72,13 +152,12 @@ pub struct Ast {
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 enum Line {
-    SubcktDecl {
-        ports: Vec<Substr>
-    },
+    SubcktDecl { name: Substr, ports: Vec<Substr> },
     Component(Component),
     EndSubckt,
 }
 
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Elem {
     /// A subcircuit declaration.
     Subckt(Subckt),
@@ -86,20 +165,32 @@ pub enum Elem {
     Component(Component),
 }
 
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
 pub struct Subckt {
+    name: Substr,
+    ports: Vec<Substr>,
     /// List of components in the subcircuit.
     components: Vec<Component>,
-    ports: Vec<Substr>,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Component {
     Mos(Mos),
+    Res(Res),
     Instance(Instance),
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Res {
+    name: Substr,
+    pos: Node,
+    neg: Node,
+    value: Substr,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Instance {
+    name: Substr,
     ports: Vec<Substr>,
     child: Substr,
     params: Params,
@@ -120,7 +211,7 @@ pub struct Mos {
     params: Params,
 }
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
 pub struct Params {
     values: HashMap<Substr, Substr>,
 }
@@ -146,7 +237,6 @@ fn is_special(c: char) -> bool {
 }
 
 pub struct Tokenizer {
-    data: Substr,
     rem: Substr,
     state: TokState,
     comment: char,
@@ -175,7 +265,6 @@ impl Tokenizer {
         let data = data.into();
         let rem = data.clone();
         Self {
-            data: Substr(data),
             rem: Substr(rem),
             state: TokState::Init,
             comment: '*',
@@ -184,9 +273,7 @@ impl Tokenizer {
     }
 
     pub fn get(&mut self) -> Option<Token> {
-        println!("GET");
         loop {
-            println!("Rem: {}", self.rem);
             self.take_ws();
             if self.rem.is_empty() {
                 if self.state == TokState::Line {
@@ -199,6 +286,7 @@ impl Tokenizer {
 
             let c = self.peek().unwrap();
             if c == '=' {
+                self.take1();
                 return Some(Token::Equals);
             }
             match self.state {
@@ -250,7 +338,7 @@ impl Tokenizer {
     }
 
     fn take_ident(&mut self) -> Substr {
-        let (rest, value) = take_till::<_, _, ()>(is_space_or_newline)(self.rem.clone()).unwrap();
+        let (rest, value) = take_till::<_, _, ()>(is_special)(self.rem.clone()).unwrap();
         self.rem = rest;
         value
     }
@@ -355,24 +443,27 @@ impl InputTakeAtPosition for Substr {
     }
 }
 
-impl<T> From<T> for Substr
-where
-    arcstr::Substr: From<T>,
-{
-    fn from(value: T) -> Self {
-        Self(arcstr::Substr::from(value))
-    }
-}
-
 impl Display for Substr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
 }
 
-impl Into<arcstr::Substr> for Substr {
-    fn into(self) -> arcstr::Substr {
-        self.0
+impl From<Substr> for arcstr::Substr {
+    fn from(value: Substr) -> Self {
+        value.0
+    }
+}
+
+impl From<&str> for Substr {
+    fn from(value: &str) -> Self {
+        Self(arcstr::Substr::from(value))
+    }
+}
+
+impl From<arcstr::Substr> for Substr {
+    fn from(value: arcstr::Substr) -> Self {
+        Self(value)
     }
 }
 
@@ -382,5 +473,26 @@ impl Token {
             Self::Ident(x) => x,
             _ => panic!("not an ident"),
         }
+    }
+}
+
+impl Params {
+    #[inline]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn insert(&mut self, k: impl Into<Substr>, v: impl Into<Substr>) {
+        self.values.insert(k.into(), v.into());
+    }
+
+    pub fn get(&self, k: &str) -> Option<&Substr> {
+        self.values.get(k)
+    }
+}
+
+impl Borrow<str> for Substr {
+    fn borrow(&self) -> &str {
+        &self.0
     }
 }
