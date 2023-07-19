7ce98c994d46c0ed8cef1f4b1665308885fe07fc
rohanku <rohankumar@berkeley.edu>
2023-07-18 20:10:16 -0700

    feat(server): add documentation to the server binary

--- libs/cache/src/bin/server.rs
+++ libs/cache/src/bin/server.rs
@@ -1,3 +1,8 @@
+//! The cache server binary.
+//!
+//! Can be configured to expose a remote or local API, or both.
+#![warn(missing_docs)]
+
 use std::{
     net::{IpAddr, SocketAddr},
     path::PathBuf,
@@ -6,17 +11,24 @@ use std::{
 use cache::{error::Result, persistent::server::Server};
 use clap::Parser;
 
+/// The arguments to the cache server binary.
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    #[clap(short = 'H', long, default_value = "0.0.0.0")]
-    host: IpAddr,
-    #[clap(short, long)]
-    remote: Option<u16>,
-    #[clap(short, long)]
-    local: Option<u16>,
-    #[clap(value_parser)]
-    root: PathBuf,
+    /// The host IP address that the configured gRPC servers should listen on.
+    #[clap(short = 'H', long, default_value = "0.0.0.0", value_hint = clap::ValueHint::Hostname)]
+    pub host: IpAddr,
+    /// The port that the local API gRPC server should listen on.
+    #[clap(short, long, value_name = "PORT")]
+    pub local: Option<u16>,
+    /// The port that the remote API gRPC server should listen on.
+    #[clap(short, long, value_name = "PORT")]
+    pub remote: Option<u16>,
+    /// The root directory of the cache server.
+    ///
+    /// All cached data and metadata will be stored in this directory.
+    #[clap(value_parser, value_hint = clap::ValueHint::DirPath)]
+    pub root: PathBuf,
 }
 
 #[tokio::main]
