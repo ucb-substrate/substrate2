b2bb95551d86504fa6c8125bedc08c45269cdbec
Rahul Kumar <rahulkumar@berkeley.edu>
2023-08-13 13:43:39 -0700

    remove opacity from enumify

--- libs/enumify/src/lib.rs
+++ libs/enumify/src/lib.rs
@@ -1,11 +1,2 @@
 pub use enumify_macros::*;
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-#[enumify]
-pub enum Opacity<O, C> {
-    /// An item whose contents cannot be inspected except in summary form as type `O`.
-    Opaque(O),
-
-    /// An item whose contents are visible to users as type `C`.
-    Clear(C),
-}
