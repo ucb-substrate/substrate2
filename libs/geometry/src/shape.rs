708053adfb9f3783fc03895ede7348ace51730f0
lazeray <65438976+lazeray@users.noreply.github.com>
2023-08-19 09:08:43 -0700

    feat(geometry): implemented contains for polygon (#292)
    * made polygon struct
    
    * bbox polygon done
    
    * Polygon setup done
    
    * removed polygon
    
    * import export gds done
    
    * docs n tests n .contains remaining
    
    * feat(geometry): Polygon added to geometry
    
    * done
    
    * fixed backslash
    
    * cargo justed
    
    * cargo checked
    
    * gogo rahul
    
    * potato
    
    * &polygon
    
    * impl contains
    
    * just check fmt
    
    * unpublic funcs
    
    * fixed bugs
    
    * added ratio to cargo toml
    
    * forgot to just fmt

--- libs/geometry/src/shape.rs
+++ libs/geometry/src/shape.rs
@@ -2,6 +2,7 @@
 
 use crate::{
     bbox::Bbox,
+    contains::{Containment, Contains},
     polygon::Polygon,
     prelude::Transform,
     rect::Rect,
@@ -85,5 +86,14 @@ impl<T: Bbox> BoundingUnion<T> for Shape {
 
     fn bounding_union(&self, other: &T) -> Self::Output {
         self.bbox().bounding_union(&other.bbox())
+    }
+}
+
+impl Contains<crate::point::Point> for Shape {
+    fn contains(&self, p: &crate::point::Point) -> Containment {
+        match self {
+            Shape::Rect(rect) => rect.contains(p),
+            Shape::Polygon(polygon) => polygon.contains(p),
+        }
     }
 }
