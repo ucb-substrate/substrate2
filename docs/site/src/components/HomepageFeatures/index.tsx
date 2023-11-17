f1c1581fdc4c7f96852fecaa279d6d3c24ecfb99
rohanku <32043201+rohanku@users.noreply.github.com>
2023-11-16 21:01:18 -0800

    fix(docs): fix dark mode SVG colors in documentation site (#319)

--- docs/site/src/components/HomepageFeatures/index.tsx
+++ docs/site/src/components/HomepageFeatures/index.tsx
@@ -43,7 +43,7 @@ function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <Svg className={clsx("featureSvg", styles.featureSvg)} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
