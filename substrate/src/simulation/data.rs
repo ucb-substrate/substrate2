6531fb9a4a98a9c1dd7b7d48007efd208ce91ec2
Rahul Kumar <rahulkumar@berkeley.edu>
2024-03-14 11:41:23 -0700

    feat(spectre): support AC simulation

--- substrate/src/simulation/data.rs
+++ substrate/src/simulation/data.rs
@@ -91,3 +91,44 @@ pub mod tran {
         }
     }
 }
+
+/// AC data definitions.
+pub mod ac {
+    use num::complex::Complex64;
+    use serde::{Deserialize, Serialize};
+    use std::ops::Deref;
+    use std::sync::Arc;
+
+    /// A series of voltage vs frequency measurements from an AC simulation.
+    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+    pub struct Voltage(pub Arc<Vec<Complex64>>);
+
+    impl Deref for Voltage {
+        type Target = Vec<Complex64>;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    /// A series of current vs frequency measurements from an AC simulation.
+    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+    pub struct Current(pub Arc<Vec<Complex64>>);
+
+    impl Deref for Current {
+        type Target = Vec<Complex64>;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    /// The frequency points associated with an AC simulation.
+    #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+    pub struct Freq(pub Arc<Vec<f64>>);
+
+    impl Deref for Freq {
+        type Target = Vec<f64>;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+}
