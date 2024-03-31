31485061e161cce89790a03cbc252e5607531ddb
rohanku <rohankumar@berkeley.edu>
2024-03-31 10:56:50 -0700

    add raw instance api

--- libs/atoll/src/lib.rs
+++ libs/atoll/src/lib.rs
@@ -298,6 +298,10 @@ impl From<Orientation> for geometry::orientation::Orientation {
 pub struct Instance<T: ExportsNestedData + ExportsLayoutData> {
     schematic: schematic::Instance<T>,
     layout: layout::Instance<T>,
+    raw: RawInstance,
+}
+
+pub struct RawInstance {
     abs: Abstract,
     /// The location of the instance in LCM units according to the
     /// top layer in the associated [`Abstract`].
@@ -305,7 +309,7 @@ pub struct Instance<T: ExportsNestedData + ExportsLayoutData> {
     orientation: Orientation,
 }
 
-impl<T: ExportsNestedData + ExportsLayoutData> Instance<T> {
+impl RawInstance {
     /// Translates this instance by the given XY-coordinates in LCM units.
     pub fn translate_mut(&mut self, p: Point) {
         self.loc += p;
@@ -369,9 +373,9 @@ impl<T: ExportsNestedData + ExportsLayoutData> Instance<T> {
     }
 
     /// Aligns this instance with another instance.
-    pub fn align_mut<T2: ExportsNestedData + ExportsLayoutData>(
+    pub fn align_mut(
         &mut self,
-        other: &Instance<T2>,
+        other: &RawInstance,
         mode: AlignMode,
         offset: i64,
     ) {
@@ -385,9 +389,9 @@ impl<T: ExportsNestedData + ExportsLayoutData> Instance<T> {
     }
 
     /// Aligns this instance with another instance with the same top layer.
-    pub fn align<T2: ExportsNestedData + ExportsLayoutData>(
+    pub fn align(
         mut self,
-        other: &Instance<T2>,
+        other: &RawInstance,
         mode: AlignMode,
         offset: i64,
     ) -> Self {
@@ -404,13 +408,6 @@ impl<T: ExportsNestedData + ExportsLayoutData> Instance<T> {
     pub fn orient(mut self, orientation: Orientation) -> Self {
         self.orient_mut(orientation);
         self
-    }
-
-    /// The ports of this instance.
-    ///
-    /// Used for node connection purposes.
-    pub fn io(&self) -> &TerminalView<<T::Io as io::schematic::HardwareType>::Bundle> {
-        self.schematic.io()
     }
 
     /// Returns the physical location of this instance.
@@ -434,6 +431,106 @@ impl<T: ExportsNestedData + ExportsLayoutData> Instance<T> {
     /// Returns the physical bounds of this instance.
     pub fn physical_bounds(&self) -> Rect {
         self.abs.physical_bounds().translate(self.physical_loc())
+    }
+}
+
+impl<T: ExportsNestedData + ExportsLayoutData> Instance<T> {
+    /// Translates this instance by the given XY-coordinates in LCM units.
+    pub fn translate_mut(&mut self, p: Point) {
+        self.raw.translate_mut(p);
+    }
+
+    /// Translates this instance by the given XY-coordinates in LCM units.
+    pub fn translate(mut self, p: Point) -> Self {
+        self.translate_mut(p);
+        self
+    }
+
+    /// Aligns this instance with another rectangle in terms of LCM units on the same
+    /// layer as the instance.
+    pub fn align_rect_mut(&mut self, orect: Rect, mode: AlignMode, offset: i64) {
+        self.raw.align_rect_mut(orect, mode, offset);
+    }
+
+    /// Aligns this instance with another rectangle in terms of LCM units on the same
+    /// layer as the instance.
+    pub fn align_rect(mut self, orect: Rect, mode: AlignMode, offset: i64) -> Self {
+        self.align_rect_mut(orect, mode, offset);
+        self
+    }
+
+    /// Aligns this instance with another instance.
+    pub fn align_mut<T2: ExportsNestedData + ExportsLayoutData>(
+        &mut self,
+        other: &Instance<T2>,
+        mode: AlignMode,
+        offset: i64,
+    ) {
+        self.raw.align_mut(&other.raw, mode, offset);
+    }
+
+    /// Aligns this instance with another instance with the same top layer.
+    pub fn align<T2: ExportsNestedData + ExportsLayoutData>(
+        mut self,
+        other: &Instance<T2>,
+        mode: AlignMode,
+        offset: i64,
+    ) -> Self {
+        self.align_mut(other, mode, offset);
+        self
+    }
+
+    /// Orients this instance in the given orientation.
+    pub fn orient_mut(&mut self, orientation: Orientation) {
+        self.raw.orient_mut(orientation);
+    }
+
+    /// Orients this instance in the given orientation.
+    pub fn orient(mut self, orientation: Orientation) -> Self {
+        self.orient_mut(orientation);
+        self
+    }
+
+    /// The ports of this instance.
+    ///
+    /// Used for node connection purposes.
+    pub fn io(&self) -> &TerminalView<<T::Io as io::schematic::HardwareType>::Bundle> {
+        self.schematic.io()
+    }
+
+    /// Returns the physical location of this instance.
+    pub fn physical_loc(&self) -> Point {
+        self.raw.physical_loc()
+    }
+
+    /// Returns the top layer of this instance.
+    pub fn top_layer(&self) -> usize {
+        self.raw.top_layer()
+    }
+
+    /// Returns the LCM bounds of this instance.
+    pub fn lcm_bounds(&self) -> Rect {
+        self.raw.lcm_bounds()
+    }
+
+    /// Returns the physical bounds of this instance.
+    pub fn physical_bounds(&self) -> Rect {
+        self.raw.physical_bounds()
+    }
+
+    /// Returns a reference to the underlying [`RawInstance`].
+    pub fn raw(&self) -> &RawInstance {
+        &self.raw
+    }
+
+    /// Returns a mutable reference to the underlying [`RawInstance`].
+    pub fn raw_mut(&mut self) -> &mut RawInstance {
+        &mut self.raw
+    }
+
+    /// Returns the underlying [`RawInstance`].
+    pub fn into_raw(self) -> RawInstance {
+        self.raw
     }
 }
 
@@ -702,9 +799,11 @@ impl<'a, PDK: Pdk + Schema> TileBuilder<'a, PDK> {
         Instance {
             layout,
             schematic,
-            abs,
-            loc: Default::default(),
-            orientation: Default::default(),
+            raw: RawInstance {
+                abs,
+                loc: Default::default(),
+                orientation: Default::default(),
+            }
         }
     }
 
@@ -760,9 +859,11 @@ impl<'a, PDK: Pdk + Schema> TileBuilder<'a, PDK> {
         Instance {
             layout,
             schematic,
-            abs,
-            loc: Default::default(),
-            orientation: Default::default(),
+            raw: RawInstance {
+                abs,
+                loc: Default::default(),
+                orientation: Default::default(),
+            }
         }
     }
 
@@ -802,11 +903,11 @@ impl<'a, PDK: Pdk + Schema> TileBuilder<'a, PDK> {
             .map(|node| self.nodes.get_mut(node).unwrap().net)
             .collect();
 
-        self.set_top_layer(instance.abs.top_layer);
+        self.set_top_layer(instance.raw.abs.top_layer);
 
         let virtual_layers = self.layout.ctx.install_layers::<crate::VirtualLayers>();
-        let orig_bbox = instance.abs.grid.slice().lcm_to_physical_rect(
-            instance.abs.grid.slice().expand_to_lcm_units(
+        let orig_bbox = instance.raw.abs.grid.slice().lcm_to_physical_rect(
+            instance.raw.abs.grid.slice().expand_to_lcm_units(
                 instance
                     .layout
                     .layer_bbox(virtual_layers.outline.id())
@@ -814,9 +915,9 @@ impl<'a, PDK: Pdk + Schema> TileBuilder<'a, PDK> {
             ),
         );
         self.abs.push(InstanceAbstract::new(
-            instance.abs,
-            instance.loc,
-            instance.orientation,
+            instance.raw.abs,
+            instance.raw.loc,
+            instance.raw.orientation,
             parent_net_ids,
         ));
 
@@ -824,11 +925,11 @@ impl<'a, PDK: Pdk + Schema> TileBuilder<'a, PDK> {
         let mut layout = instance.layout;
         layout.transform_mut(Transformation::from_offset_and_orientation(
             Point::zero(),
-            instance.orientation,
+            instance.raw.orientation,
         ));
         let new_bbox = orig_bbox.transform(Transformation::from_offset_and_orientation(
             Point::zero(),
-            instance.orientation,
+            instance.raw.orientation,
         ));
         layout.translate_mut(
             orig_bbox.corner(Corner::LowerLeft) - new_bbox.corner(Corner::LowerLeft) + physical_loc,
