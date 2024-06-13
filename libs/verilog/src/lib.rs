236b06898f0b5ad7c4fe3b3c4f3ae321142d02e9
rohanku <rohankumar@berkeley.edu>
2024-06-13 14:01:55 -0700

    add more helper functions

--- libs/verilog/src/lib.rs
+++ libs/verilog/src/lib.rs
@@ -1,6 +1,7 @@
 use scir::schema::Schema;
 use scir::{CellId, Library};
 use std::io::Write;
+use std::path::Path;
 
 pub fn export_verilog_shells<S: Schema, W: Write>(
     lib: &Library<S>,
@@ -40,4 +41,29 @@ pub fn export_verilog_shells_by_name<S: Schema, N: AsRef<str>, W: Write>(
         .map(|cell| lib.cell_id_named(cell.as_ref()))
         .collect::<Vec<_>>();
     export_verilog_shells(lib, &ids, out)
+}
+
+pub fn export_verilog_shells_to_file<S: Schema, P: AsRef<Path>>(
+    lib: &Library<S>,
+    cells: &[CellId],
+    path: P,
+) -> std::io::Result<()> {
+    let path = path.as_ref();
+    if let Some(parent) = path.parent() {
+        std::fs::create_dir_all(parent)?;
+    }
+    let mut f = std::fs::File::create(path)?;
+    export_verilog_shells(lib, cells, &mut f)
+}
+
+pub fn export_verilog_shells_by_name_to_file<S: Schema, N: AsRef<str>, P: AsRef<Path>>(
+    lib: &Library<S>,
+    cells: &[N],
+    path: P,
+) -> std::io::Result<()> {
+    let ids = cells
+        .iter()
+        .map(|cell| lib.cell_id_named(cell.as_ref()))
+        .collect::<Vec<_>>();
+    crate::export_verilog_shells_to_file(lib, &ids, path)
 }
