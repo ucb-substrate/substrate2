2a95cb8558d5fb44ae0a958c3cbdf0287950b33b
Rahul Kumar <rahulkumar@berkeley.edu>
2024-12-19 23:03:46 -0800

    re add EXAMPLES_PATH

--- tools/quantus/src/lib.rs
+++ tools/quantus/src/lib.rs
@@ -29,6 +29,7 @@ pub struct RuleCheck {
 #[cfg(test)]
 pub mod tests {
     pub const TEST_BUILD_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/build");
+    pub const EXAMPLES_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../pegasus/examples");
     pub const COLBUF_LAYOUT_PATH: &str = concat!(
         env!("CARGO_MANIFEST_DIR"),
         "/../../examples2/colbuf/test_col_buffer_array.gds"
