0bf48b5e90fe5b0a167ba8ca2c5bf68863097358
Rohan Kumar <rohankumar@berkeley.edu>
2024-12-26 14:50:35 -0800

    work on versioning

--- docs/docusaurus/docusaurus.config.ts
+++ docs/docusaurus/docusaurus.config.ts
@@ -1,6 +1,10 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import * as fs from 'fs';
+import toml from '@iarna/toml';
+
+const siteConfig = toml.parse(fs.readFileSync('../site-config.toml', 'utf-8'));
 
 const config: Config = {
   title: 'Substrate Labs',
@@ -11,7 +15,7 @@ const config: Config = {
   url: 'https://docs.substratelabs.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: siteConfig.docusaurus.base_url,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -29,6 +33,8 @@ const config: Config = {
     locales: ['en'],
   },
 
+  onBrokenLinks: 'ignore',
+
   plugins: ['./src/plugins/substrate-source-assets'],
 
   presets: [
@@ -41,20 +47,18 @@ const config: Config = {
           versions: {
             current: {
               label: 'main',
-              path: 'main',
+              banner: siteConfig.docusaurus.banner,
             },
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/substrate-labs/substrate2/tree/main/docs/site',
+          editUrl: siteConfig.docusaurus.edit_url,
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/substrate-labs/substrate2/tree/main/docs/site',
+          editUrl: siteConfig.docusaurus.edit_url
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -75,8 +79,8 @@ const config: Config = {
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            type: 'docsVersion',
+            to: "/docs",
             position: 'left',
             label: 'Documentation',
           },
@@ -85,11 +89,28 @@ const config: Config = {
             label: 'API',
             position: 'left',
           },
-          {
-            type: 'docsVersionDropdown',
-            position: 'right',
-            dropdownActiveClassDisabled: true,
-          },
+          {href: '/blog', autoAddBaseUrl: false, label: 'Blog', position: 'left'},
+          { type: 'docsVersionDropdown', position: 'right' },
+          // {
+          //   type: 'dropdown',
+          //   label: siteConfig.docusaurus.current_version,
+          //   position: 'right',
+          //   items: [
+          //     {
+          //       label: 'release',
+          //       href: '/docs',
+          //       autoAddBaseUrl: false,
+          //     },
+          //     // ... more items
+          //   ].concat(
+          //     siteConfig.site.versions.map((version) => {
+          //     return {
+          //       label: version,
+          //       href: `/${version}/docs`,
+          //       autoAddBaseUrl: false,
+          //     }}))
+          //   ,
+          // },
           {
             href: 'https://github.com/substrate-labs/substrate2',
             label: 'GitHub',
--- docs/sitebuilder/src/paths.rs
+++ docs/sitebuilder/src/paths.rs
@@ -1,3 +1,5 @@
 pub const BUILD_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/build");
+pub const BUILT_SITE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/build/__site");
+pub const CONFIG_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/site-config.toml");
 pub const SUBSTRATE_CLONE_URL: &str = "git@github.com:ucb-substrate/substrate2.git";
 pub const SUBSTRATE_EDIT_URL: &str = "https://github.com/ucb-substrate/substrate2";
