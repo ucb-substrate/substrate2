f4b4f7c0bb1721c92292856dca86ae10c81215b8
Rahul Kumar <rahulkumar@berkeley.edu>
2025-01-17 11:46:55 -0800

    initial inverter layout

--- examples/latest/sky130_inverter/src/layout.rs
+++ examples/latest/sky130_inverter/src/layout.rs
@@ -1,6 +1,7 @@
 use layir::Shape;
 use sky130pdk::{
     layers::Sky130Layer,
+    layout::{NtapTile, PtapTile},
     mos::{GateDir, MosLength, NmosTile, PmosTile},
     Sky130Pdk,
 };
@@ -11,6 +12,7 @@ use substrate::{
         point::Point,
         prelude::Transformation,
         rect::Rect,
+        span::Span,
         transform::{TransformMut, TranslateMut},
         union::BoundingUnion,
     },
@@ -52,14 +54,50 @@ impl Layout for Inverter {
         );
         cell.draw(din.clone())?;
 
+        let mut ntap = cell.generate(NtapTile::new(2, 2));
+        ntap.align_mut(AlignMode::Above, ntap.bbox_rect(), pmos.bbox_rect(), 0);
+        ntap.align_mut(
+            AlignMode::CenterHorizontal,
+            ntap.bbox_rect(),
+            pmos.bbox_rect(),
+            0,
+        );
+
+        let mut ptap = cell.generate(PtapTile::new(2, 2));
+        ptap.align_mut(AlignMode::Beneath, ptap.bbox_rect(), nmos.bbox_rect(), 0);
+        ptap.align_mut(
+            AlignMode::CenterHorizontal,
+            ptap.bbox_rect(),
+            nmos.bbox_rect(),
+            0,
+        );
+
+        let vdd = ntap.io().vpb.primary.clone();
+        let vss = ptap.io().vnb.primary.clone();
+
+        let nmos_s = nmos.io().sd[0].bbox_rect();
+        let vss_conn = Rect::from_spans(
+            nmos_s.hspan(),
+            Span::new(vss.bbox_rect().bot(), nmos_s.top()),
+        );
+        cell.draw(Shape::new(Sky130Layer::Li1, vss_conn))?;
+
+        let pmos_s = pmos.io().sd[0].bbox_rect();
+        let vdd_conn = Rect::from_spans(
+            pmos_s.hspan(),
+            Span::new(pmos_s.bot(), vdd.bbox_rect().top()),
+        );
+        cell.draw(Shape::new(Sky130Layer::Li1, vdd_conn))?;
+
+        cell.draw(ntap)?;
+        cell.draw(ptap)?;
         cell.draw(nmos)?;
         cell.draw(pmos)?;
 
-        let tmp = Shape::new(Sky130Layer::Met1, Rect::from_sides(0, 0, 100, 100));
         Ok((
             InverterIoView {
-                vdd: PortGeometry::new(tmp.clone()),
-                vss: PortGeometry::new(tmp.clone()),
+                vdd: PortGeometry::new(vdd),
+                vss: PortGeometry::new(vss),
                 din: PortGeometry::new(din),
                 dout: PortGeometry::new(dout),
             },
