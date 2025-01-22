5b4e7c2c9fce3044f3d7e8cc6e9f5c5d302ff83b
Rohan Kumar <rohankumar@berkeley.edu>
2025-01-22 10:25:23 -0800

    fix(docs): fix snippet publishing

--- codegen/build.rs
+++ codegen/build.rs
@@ -1,10 +1,7 @@
 use snippets::build_snippets;
 
 fn main() {
-    let example_path = concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/../examples/latest/substrate_api_examples/src/lib.rs"
-    );
+    let example_path = examples::get_path("latest/substrate_api_examples/src/lib.rs");
     println!("cargo::rerun-if-changed={example_path}");
     build_snippets(example_path, "substrate");
 }
