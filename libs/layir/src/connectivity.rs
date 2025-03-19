43f1de36ce3383afcb057f57c934596db802ee2b
Sidharth Niwas Sharma <snsharma@berkeley.edu>
2025-03-19 02:32:22 -0700

    added djs

--- libs/layir/src/connectivity.rs
+++ libs/layir/src/connectivity.rs
@@ -1,8 +1,6 @@
 use geometry::bbox::Bbox;
 use geometry::rect::{self, Rect};
-use std::rc::Rc;
-use std::cell::RefCell;
-
+use aph_disjoint_set::DisjointSet;
 use crate::CellId;
 use crate::Instance;
 
@@ -72,9 +70,11 @@ trait Connectivity: Sized + PartialEq {
             }
         }
 
-        for inst in Self::get_cells(cell, lib) {
-            for shape in Self::flatten_cell(inst, lib) {
-                ret.push(shape);
+        let list_of_cells = Self::get_cells(cell, lib);
+
+        for inst in list_of_cells.into_iter() {
+            for thing in Self::flatten_cell(inst, lib).into_iter() {
+                ret.push(thing);
             }
         }
 
@@ -95,59 +95,51 @@ trait Connectivity: Sized + PartialEq {
         return false;
     }
 
-    fn vec_union<'a>(
-        vec1 : Vec<&'a Shape<Self>>,
-        vec2 : Vec<&'a Shape<Self>>,
-    ) -> Vec<&'a Shape<Self>> {
-        let mut ret = vec1.clone();
-        let temp_vec2 = vec2.clone();
-
-        for thing in temp_vec2.into_iter() {
-            if !(Self::vec_has(thing, &ret)) {
-                ret.push(thing);
-            }
-        }
-        ret
-    }
-
     fn connected_components<'a>(
         cell : &'a Cell<Self>,
         lib : &'a LibraryBuilder<Self>,
-    ) -> Vec<(&'a Shape<Self>, Rc<RefCell<Rc<RefCell<Vec<&'a Shape<Self>>>>>>)> {
+    ) -> (Vec<&'a Shape<Self>>, Vec<Vec<&'a Shape<Self>>>) {
+        
         let all_shapes = Self::flatten_cell(cell, lib);
-        let mut ret_refs: Vec<(&'a Shape<Self>, Rc<RefCell<Rc<RefCell<Vec<&'a Shape<Self>>>>>>)> = vec![];
 
-        for thing in all_shapes.clone().into_iter() {
-            let temp_vec = Rc::new(RefCell::new(Rc::new(RefCell::new(vec![thing]))));
-            ret_refs.push((thing, temp_vec));
-        }
+        let mut djs  = DisjointSet::new(all_shapes.len());
 
         for (start_index, start_shape) in all_shapes.clone().into_iter().enumerate() {
             for (part_index, part_shape) in all_shapes.clone().into_iter().enumerate() {
                 if start_index != part_index {
                     if Self::intersect_shapes(start_shape, part_shape) && start_shape.layer().connected(part_shape.layer()) {
+                        djs.union(start_index, part_index);
+                    }
+                }
+            }
+        }
 
-                        let start_vec = &mut *ret_refs[start_index].1.borrow_mut();
-                        let part_vec = &mut *ret_refs[part_index].1.borrow_mut();
-
-                        let temp_vec : Rc<RefCell<Vec<&Shape<Self>>>> = Rc::new(RefCell::new(Self::vec_union(start_vec.borrow().clone(), part_vec.borrow().clone())));
-
-                        *start_vec = temp_vec.clone();
-                        *part_vec = temp_vec.clone();
-           
+        let mut ret : Vec<Vec<&Shape<Self>>> = vec![vec![]; all_shapes.clone().len()];
+        
+        for (start_index, start_shape) in all_shapes.clone().into_iter().enumerate() {
+            for (part_index, part_shape) in all_shapes.clone().into_iter().enumerate() {
+                if djs.is_united(start_index, part_index) {
+                    if !(Self::vec_has(part_shape, &ret[start_index])) {
+                        ret[start_index].push(part_shape);
+                    }
+                    if !(Self::vec_has(start_shape, &ret[part_index])) {
+                        ret[part_index].push(start_shape);
                     }
                 }
             }
         }
 
 
-        ret_refs
+
+        (all_shapes, ret)
     }
 
 }
 
 #[cfg(test)]
 mod tests {
+    use std::io::empty;
+
     use geometry::rect::Rect;
 
     use crate::LibraryBuilder;
@@ -207,28 +199,44 @@ mod tests {
 
     #[test]
     fn test_complete() {
-        let mut big_cell : Cell<Layer> = Cell::new("test");
-        let mut small_cell : Cell<Layer> = Cell::new("test2");
+        let mut big_cell : Cell<Layer> = Cell::new("big cell test");
+        let mut small_cell : Cell<Layer> = Cell::new("small cell test");
         let mut lib : LibraryBuilder<Layer> = LibraryBuilder::new();
 
+        lib.add_cell(small_cell.clone());
+
+        let small_cell_id = lib.cells().next().unwrap().0;
+
         lib.add_cell(big_cell.clone());
+
+        let small_cell_instance = Instance::new(small_cell_id, "small_cell_test");
+        big_cell.add_instance(small_cell_instance);
+
+        
 
         let m1_shape = Shape::new(Layer::Met1, Rect::from_sides(0, 0, 100, 100));
         let v1_shape = Shape::new(Layer::Via1, Rect::from_sides(10, 10, 100, 100));
         let m2_shape = Shape::new(Layer::Met2, Rect::from_sides(10, 10, 50, 50));
+        let m3_shape = Shape::new(Layer::Met2, Rect::from_sides(1000, 1000, 5000, 5000));
+        let m4_shape = Shape::new(Layer::Met2, Rect::from_sides(0, 0, 30, 30));
         
         big_cell.add_element(m1_shape.clone());
         big_cell.add_element(v1_shape.clone());
         big_cell.add_element(m2_shape.clone());
+        big_cell.add_element(m3_shape.clone());
+        small_cell.add_element(m4_shape.clone());
 
-        assert_eq!(Layer::connected_shapes(&big_cell, &m1_shape), vec![&m1_shape, &v1_shape]);
-        assert_eq!(Layer::connected_shapes(&big_cell, &v1_shape), vec![&m1_shape, &v1_shape, &m2_shape]);
-        assert_eq!(Layer::connected_shapes(&big_cell, &m2_shape), vec![&v1_shape, &m2_shape]);
+        let asdf = Layer::get_cells(&big_cell, &lib);
+        assert_eq!(asdf, vec![&small_cell]);
 
         let x = Layer::connected_components(&big_cell, &lib);
 
-        let mut asdf = x[0].1.borrow().borrow().clone();
-        assert_eq!(x[0].1.borrow().borrow().clone(), vec![&m1_shape, &v1_shape, &m2_shape]);
-        
+        assert_eq!(x.0, vec![&m1_shape, &v1_shape, &m2_shape, &m3_shape, &m4_shape]);
+        assert_eq!(x.1[0], vec![&m1_shape, &v1_shape, &m2_shape]);
+        assert_eq!(x.1[1], vec![&m1_shape, &v1_shape, &m2_shape]);
+        assert_eq!(x.1[2], vec![&m1_shape, &v1_shape, &m2_shape]);
+        assert_eq!(x.1[3], vec![&m3_shape]);
+        assert_eq!(x.1[4], vec![&m1_shape, &v1_shape, &m2_shape, &m4_shape]);
+
     }
 }
