5cad55bb90224b805ab49b8e3f26e6d3875ae374
Rohan Kumar <rohankumar@berkeley.edu>
2025-04-18 15:36:07 -0700

    use instance for resistor

--- pdks/sky130/src/lib.rs
+++ pdks/sky130/src/lib.rs
@@ -431,8 +431,9 @@ impl FromSchema<Sky130SrcNdaSchema> for Spice {
                     ),
                 ]),
             },
-            Primitive::PrecisionResistor(res) => spice::Primitive::Res2 {
-                value: spice::ComponentValue::Model("xuhrpoly_1p41".into()),
+            Primitive::PrecisionResistor(res) => spice::Primitive::RawInstance {
+                cell: "xuhrpoly_1p41".into(),
+                ports: vec!["1".into(), "2".into()],
                 params: HashMap::from_iter([
                     (
                         UniCase::new(arcstr::literal!("w")),
