ae3f752aea429f19093b9985f05877704c6d3002
CONnor9029 <connorlu@gmail.com>
2025-06-16 21:18:21 -0700

    fixed comments, used bbox_rect, ran rustfmt, made functions not exposed to public api, use hashsets(in progress

--- libs/layir/src/connectivity.rs
+++ libs/layir/src/connectivity.rs
@@ -1,12 +1,88 @@
-use geometry::bbox::Bbox;
-use geometry::rect::{self, Rect};
-use aph_disjoint_set::DisjointSet;
 use crate::CellId;
 use crate::Instance;
+use aph_disjoint_set::DisjointSet;
+use geometry::bbox::Bbox;
+use geometry::rect::{self, Rect};
+use std::collections::HashSet;
+use std::hash::Hash;
 
-use crate::{Cell, Library, Shape, LibraryBuilder, Transformation, TransformMut, TransformRef};
 use crate::Element;
-trait Connectivity: Sized + PartialEq + Clone{
+use crate::{Cell, Library, LibraryBuilder, Shape, TransformMut, TransformRef, Transformation};
+
+/// Returns true if two shapes overlap on a flat plane, and false otherwise.
+fn intersect_shapes<'a, 'b, T: Clone>(shape1: &'b Shape<T>, shape2: &'b Shape<T>) -> bool {
+    let shape1_bbox: Rect = shape1.bbox_rect();
+    let shape2_bbox: Rect = shape2.bbox_rect();
+    shape1_bbox.intersection(shape2_bbox) != None
+}
+
+/// Returns a vector of all sub-cells in a given cell.
+fn get_cells<'a, T: Clone>(cell: &'a Cell<T>, lib: &'a Library<T>) -> Vec<&'a Cell<T>> {
+    let mut ret: Vec<&'a Cell<T>> = vec![];
+
+    for inst_pair in cell.instances() {
+        let inst: &Instance = inst_pair.1;
+        let cellid: CellId = inst.child();
+        ret.push(lib.cell(cellid));
+    }
+    ret
+}
+
+/// Return a vector of pointers to all transformed sub-elements/shapes from a single root shape.
+fn flatten_instance<'b: 'a, 'a, T>(inst: &'b Instance, lib: &'b Library<T>) -> Vec<&'a Shape<T>>
+where
+    T: Connectivity,
+{
+    let mut ret: Vec<&'a Shape<T>> = vec![];
+
+    let cellid: CellId = inst.child();
+    let transform: Transformation = inst.transformation();
+
+    let cell_ref_temp: Vec<_> = lib.cell(cellid).elements().collect();
+
+    for elem in cell_ref_temp {
+        if let Element::Shape(shape) = elem {
+            ret.push(&shape);
+        }
+    }
+
+    let new_cell_ref_temp: Vec<_> = lib.cell(cellid).instances().collect();
+
+    for instance in new_cell_ref_temp {
+        ret.append(&mut flatten_instance::<T>(instance.1, lib));
+    }
+
+    for sh in ret.clone().into_iter() {
+        let shap = sh.clone();
+        shap.transform_ref(transform);
+    }
+
+    ret
+}
+
+/// Returns a recursively generated 1-d vector of sub-shapes in a given parent cell.
+fn flatten_cell<'a, T>(cell: &'a Cell<T>, lib: &'a Library<T>) -> Vec<&'a Shape<T>>
+where
+    T: Connectivity,
+{
+    let mut ret: Vec<&'a Shape<T>> = vec![];
+
+    for elem in cell.elements() {
+        if let Element::Shape(shape) = elem {
+            ret.push(shape);
+        }
+    }
+
+    for inst in cell.instances() {
+        for thing in flatten_instance::<T>(inst.1, lib) {
+            ret.push(&thing);
+        }
+    }
+
+    ret
+}
+
+pub trait Connectivity: Sized + PartialEq + Clone + Hash {
     fn connected_layers(&self) -> Vec<Self>;
 
     /// Returns a vector of layers connected to a given layer.
@@ -14,143 +90,42 @@ trait Connectivity: Sized + PartialEq + Clone{
         self.connected_layers().contains(other)
     }
 
-    /// Returns true if two shapes overlap on a flat plane, and false otherwise.
-    fn intersect_shapes<'a, 'b>(
-        shape1 : &'b Shape<Self>,
-        shape2 : &'b Shape<Self>,
-    ) -> bool {
-        let shape1_bbox : Rect = shape1.bbox().expect("error");
-        let shape2_bbox : Rect = shape2.bbox().expect("error");
-        shape1_bbox.intersection(shape2_bbox) != None
-    }
-
-    /// Returns a vector of all sub-cells in a given cell.
-    fn get_cells<'a>(
-        cell : &'a Cell<Self>,
-        lib : &'a Library<Self>,
-    ) -> Vec<&'a Cell<Self>> {
-        let mut ret : Vec<&'a Cell<Self>> = vec![];
-
-        for inst_pair in cell.instances() {
-            let inst : &Instance = inst_pair.1;
-            let cellid : CellId = inst.child();
-            ret.push(lib.cell(cellid));
-        }
-        ret
-    }
-
-    /// Return a vector of pointers to all transformed sub-elements/shapes from a single root shape.
-    fn flatten_instance<'b : 'a, 'a>(
-        inst: &'b Instance,
-        lib: &'b Library<Self>,
-    ) -> Vec<&'a Shape<Self>> {
-
-        let mut ret : Vec<&'a Shape<Self>> = vec![];
-
-        let cellid : CellId = inst.child();
-        let transform : Transformation = inst.transformation();
-        
-        let cell_ref_temp : Vec<_>= lib.cell(cellid).elements().collect();
-
-        for elem in cell_ref_temp {
-            if let Element::Shape(shape) = elem {
-                ret.push(&shape);
-            }  
-        }
-
-        let new_cell_ref_temp : Vec<_> = lib.cell(cellid).instances().collect();;
-
-        for instance in new_cell_ref_temp {
-            ret.append(&mut Self::flatten_instance(instance.1, lib));
-        }
-        
-
-        for sh in ret.clone().into_iter() {
-            let shap = sh.clone();
-            shap.transform_ref(transform);
-        }
-
-        ret
-    }
-
-
-    /// Returns a recursively generated 1-d vector of sub-shapes in a given parent cell.
-    fn flatten_cell<'a>(
-        cell: &'a Cell<Self>,
-        lib : &'a Library<Self>,
-    ) -> Vec<&'a Shape<Self>> {
-        let mut ret : Vec<&'a Shape<Self>> = vec![];
-
-        for elem in cell.elements() {
-            if let Element::Shape(shape) = elem {
-                ret.push(shape);
-            }
-        }
-       
-        for inst in cell.instances() {
-            for thing in Self::flatten_instance(inst.1, lib) {
-                ret.push(&thing);
-            }
-        }
-
-        ret
-    }
-
-    /// Checks if a vector of shapes contains a given shape.
-    fn contains_shape<'a>(
-        target_shape : &'a Shape<Self>,
-        list : &Vec<&'a Shape<Self>>,
-    ) -> bool {
-        let temp_list = list.clone();
-        for shape in temp_list.into_iter() {
-            if shape == target_shape {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /// Returns a vector containing vectors of shapes connected to each sub-shape in a given cell
+    /// Returns a vector containing hashsets of shapes connected to each sub-shape in a given cell.
     fn connected_components<'a>(
-        cell : &'a Cell<Self>,
-        lib : &'a Library<Self>,
-    ) -> (Vec<&'a Shape<Self>>, Vec<Vec<&'a Shape<Self>>>) {
-    
-        let all_shapes = Self::flatten_cell(cell, lib); // all sub-shapes contained in given cell
+        cell: &'a Cell<Self>,
+        lib: &'a Library<Self>,
+    ) -> (Vec<HashSet<&'a Shape<Self>>>) {
+        // All sub-shapes contained in given cell
+        let all_shapes = flatten_cell::<Self>(cell, lib);
+        let mut djs = DisjointSet::new(all_shapes.len());
 
-        let mut djs  = DisjointSet::new(all_shapes.len());
-
-        // build disjoint sets
+        // Build disjoint sets
         for (start_index, start_shape) in all_shapes.clone().into_iter().enumerate() {
             for (other_index, other_shape) in all_shapes.clone().into_iter().enumerate() {
                 if start_index != other_index {
-                    if Self::intersect_shapes(start_shape, other_shape) && start_shape.layer().connected(other_shape.layer()) {
+                    if intersect_shapes::<Self>(start_shape, other_shape)
+                        && start_shape.layer().connected(other_shape.layer())
+                    {
                         djs.union(start_index, other_index);
                     }
                 }
             }
         }
-
-        let mut ret : Vec<Vec<&Shape<Self>>> = vec![vec![]; all_shapes.clone().len()]; 
-        // ret is a vector of vectors of shapes connected to the shapes in all_shapes
-        
-        // build vectors of connected shapes to return
+        // Ret is a vector of hashsets of shapes connected to the shapes in the referenced cell
+        //let mut ret: Vec<HashSet<&Shape<Self>>> = vec![HashSet![]; all_shapes.clone().len()];
+        let mut ret: Vec<HashSet<&Shape<Self>>> =
+            (0..all_shapes.len()).map(|_| HashSet::new()).collect();
+        // Build hashsets of connected shapes to return
         for (start_index, start_shape) in all_shapes.clone().into_iter().enumerate() {
             for (other_index, other_shape) in all_shapes.clone().into_iter().enumerate() {
                 if djs.is_united(start_index, other_index) {
-                    if !(Self::contains_shape(other_shape, &ret[start_index])) {
-                        ret[start_index].push(other_shape);
-                    }
-                    if !(Self::contains_shape(start_shape, &ret[other_index])) {
-                        ret[other_index].push(start_shape);
-                    }
+                    ret[start_index].insert(other_shape);
+                    ret[other_index].insert(start_shape);
                 }
             }
         }
-
-        (all_shapes, ret)
+        ret
     }
-
 }
 
 #[cfg(test)]
@@ -182,15 +157,14 @@ mod tests {
 
     #[test]
     fn test_complete() {
-        let mut big_cell : Cell<Layer> = Cell::new("big cell test");
-        let mut small_cell : Cell<Layer> = Cell::new("small cell test");
-        let mut lib : LibraryBuilder<Layer> = LibraryBuilder::new();
+        let mut small_cell: Cell<Layer> = Cell::new("small cell test");
+        let mut big_cell: Cell<Layer> = Cell::new("big cell test");
+        let mut lib: LibraryBuilder<Layer> = LibraryBuilder::new();
 
-        let m4_shape = Shape::new(Layer::Met2, Rect::from_sides(0, 0, 30, 30));
-        small_cell.add_element(m4_shape.clone());
+        let m2_shape1 = Shape::new(Layer::Met2, Rect::from_sides(0, 0, 30, 30));
+        small_cell.add_element(m2_shape1.clone());
 
         lib.add_cell(small_cell.clone());
-
         let small_cell_id = lib.cells().next().unwrap().0;
 
         lib.add_cell(big_cell.clone());
@@ -198,29 +172,29 @@ mod tests {
         let small_cell_instance = Instance::new(small_cell_id, "small_cell_test");
         big_cell.add_instance(small_cell_instance);
 
-        
-
         let m1_shape = Shape::new(Layer::Met1, Rect::from_sides(0, 0, 100, 100));
         let v1_shape = Shape::new(Layer::Via1, Rect::from_sides(10, 10, 100, 100));
-        let m2_shape = Shape::new(Layer::Met2, Rect::from_sides(10, 10, 50, 50));
-        let m3_shape = Shape::new(Layer::Met2, Rect::from_sides(1000, 1000, 5000, 5000));
-        
+        let m2_shape2 = Shape::new(Layer::Met2, Rect::from_sides(10, 10, 50, 50));
+        let m2_shape3 = Shape::new(Layer::Met2, Rect::from_sides(1000, 1000, 5000, 5000));
+
         big_cell.add_element(m1_shape.clone());
         big_cell.add_element(v1_shape.clone());
-        big_cell.add_element(m2_shape.clone());
-        big_cell.add_element(m3_shape.clone());
+        big_cell.add_element(m2_shape2.clone());
+        big_cell.add_element(m2_shape3.clone());
 
         let binding = lib.clone().build().unwrap();
-        let big_cell_cells = Layer::get_cells(&big_cell, &binding);
+        let big_cell_cells = get_cells(&big_cell, &binding);
 
         let x = Layer::connected_components(&big_cell, &binding);
 
-        assert_eq!(x.0, vec![&m1_shape, &v1_shape, &m2_shape, &m3_shape, &m4_shape]);
-        assert_eq!(x.1[0], vec![&m1_shape, &v1_shape, &m2_shape, &m4_shape]);
-        assert_eq!(x.1[1], vec![&m1_shape, &v1_shape, &m2_shape, &m4_shape]);
-        assert_eq!(x.1[2], vec![&m1_shape, &v1_shape, &m2_shape, &m4_shape]);
-        assert_eq!(x.1[3], vec![&m3_shape]);
-        assert_eq!(x.1[4], vec![&m1_shape, &v1_shape, &m2_shape, &m4_shape]);
-
+        assert_eq!(
+            x.0,
+            vec![&m1_shape, &v1_shape, &m2_shape2, &m2_shape3, &m2_shape1]
+        );
+        assert_eq!(x.1[0], vec![&m1_shape, &v1_shape, &m2_shape2, &m2_shape1]);
+        assert_eq!(x.1[1], vec![&m1_shape, &v1_shape, &m2_shape2, &m2_shape1]);
+        assert_eq!(x.1[2], vec![&m1_shape, &v1_shape, &m2_shape2, &m2_shape1]);
+        assert_eq!(x.1[3], vec![&m2_shape3]);
+        assert_eq!(x.1[4], vec![&m1_shape, &v1_shape, &m2_shape2, &m2_shape1]);
     }
 }
